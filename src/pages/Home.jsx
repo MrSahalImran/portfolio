e3c73a3ab76497ee2cd3efe328cda2300e48a091
sahal <sahalimran.pro@gmail.com>
2025-08-20 20:29:56 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import SkillSection from "../components/SkillSection";
+import { ProjectsSection } from "../components/ProjectSection";
+import ContactSection from "../components/ContactSection";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* theme toggle */}
+      <ThemeToggle />
+      {/* background effect */}
+      <StarBackground />
+      {/* navbar */}
+      <Navbar />
+      {/* main content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Home;
