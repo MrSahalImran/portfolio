e4eed1a9295b1a83ef166304fdf83c90d0e16c48
sahal <sahalimran.pro@gmail.com>
2025-08-21 20:26:26 +0530

    Minor fixes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ const AboutSection = () => {
               </a>
 
               <a
-                href="/Sahal_Imran_resume.pdf"
+                href="/Sahal_imran_resume.pdf"
                 download="Sahal_Imran_resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
